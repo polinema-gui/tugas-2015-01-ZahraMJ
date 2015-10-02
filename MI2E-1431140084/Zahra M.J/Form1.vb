8a9d68e387aad998f33c0cf623b9474c0fdface3
ZahraMJ <zahramj3@gmail.com>
2015-10-02 18:04:21 +0700

    Tugas mengubah warna form

---
+++ MI2E-1431140084/Zahra M.J/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If MyBase.BackColor = Color.BlueViolet Then
+            MyBase.BackColor = Color.LightBlue
+        Else
+            MyBase.BackColor = Color.BlueViolet
+        End If
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
